2d3eae742c66fae9db69e1c2ea830a39c597d0c7
arianne <ariannealonso8@gmail.com>
2025-11-28 22:27:08 -0300

    a

--- web/Backend/src/interfaces/RequestLike.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from "./User";
-
-export interface RequestLike {
-  cookies: { authToken?: string };
-  session: { token?: string };
-  user?: User;
-}
--- web/Backend/src/interfaces/ResponseLike.ts
+++
@@ -1,6 +0,0 @@
-import type { ValidateSessionResponse } from "../interfaces/ValidateSessionResponse";
-
-export interface ResponseLike {
-  status: (code: number) => { json: (data: ValidateSessionResponse) => any };
-  json: (data: ValidateSessionResponse) => any;
-}
