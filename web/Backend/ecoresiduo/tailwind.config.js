fee8e78be56c127e63ae58b87291835bc67c032a
arianne <ariannealonso8@gmail.com>
2025-11-28 22:25:40 -0300

    se agrego el backend

---
+++ web/Backend/ecoresiduo/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./main.html",
+    "./mapa.html",
+    "./consejos.html",
+    "./perfil.html",
+    "./register.html",
+    "./estadistica.html",
+    "./admin-events.html",
+    "./*.html",                    // todos los HTML en la raíz
+    "./src/**/*.{js,ts,jsx,tsx}"   // todos los JS dentro de src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
